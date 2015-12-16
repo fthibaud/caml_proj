b04407ece1902d74da9a713000ae3d02fee929bf
fthibaud <thib31@gmail.com>
2015-12-16 11:00:03 +0100

    A

--- position.ml
+++ position.ml
@@ -134,7 +134,7 @@ let pos_circle = fun radius dirvector tgpoint center distance ->
 let norme = fun v ->
   let (d1,d2) = v in
   sqrt (d1**2. +. d2**2.);;
-  
+
 (*fonction renvoyant la position après avoir avancé tout droit*)
 let pos_vector = fun dirvector tgpoint distance ->
   let (t1,t2) = tgpoint in
@@ -144,7 +144,42 @@ let pos_vector = fun dirvector tgpoint distance ->
   let (x,y) = (u+.t1,v+.t2) in
   (x,y);;
 
-(*fonction qui prend une traj et qui check intersection avec un nuage*)
+(*fonction qui teste vérifie qu'un point n'appartient pas à une géométrie*)
+let not_in_geom = fun geom point ->
+  match geom with
+    Circle ((c1,c2),r) ->
+      begin
+        let (x,y) = point in
+        let dist = sqrt ((x-.c1)**2. +. (y-.c2)**2.) in
+        dist > r;
+      end
+  | Polygon l ->
+      begin
+        let (sumx,sumy) =  List.fold_left (fun (x1,y1) (x2,y2) -> (x1+.x2,y1+.y2) ) (0.,0.) l in
+        let n = List.length l in
+        let bary = (sumx/.(float n) , sumy/.(float n)) in
+        let d = ref [] in
+
+        (* recherche coeff m et p de la droite de la forme y=mx+p puis évaluation en un point *)
+        let eval1 = fun l point i ->
+          let (x,y) = point in
+          let (xa,ya) = List.nth l i in
+          let (xb,yb) = List.nth l (i+1) in
+          if xa=xb then (x-.xa)
+          else
+            begin
+              let m = (yb-.ya)/.(xb-.xa) in
+              let p = ya -. m*.xa in
+              (m*.x+.p-.y)
+            end in
+        
+        for i=0 to n-1 do
+          d := (eval1 l bary i, eval1 l point i)::!d done;
+        let p = fun x ->
+          let (a,b) = x in
+          a *. b <= 0. in
+        List.exists p !d
+      end;;
 
 
 (*----Fonctions spécifiques----*)
@@ -172,7 +207,11 @@ let nextpos = fun planepos arrival plane dt ->
   !nextpos_list;;
 
 (*fonction qui élimine les positions suivantes possibles si elles sont dans un nuage*)
-let not_in_clouds = fun 
+let not_in_clouds = fun nextpos_list clouds -> (*not_in_geom point geom*)
+  let not_in_cloud = fun pos ->
+    List.for_all (not_in_geom pos) clouds
+  in
+  List.filter not_in_cloud nextpos_list;;
 
 (*fonction qui imprime une liste de coordonnées*)
 let print_point_list = fun point_list ->
@@ -184,10 +223,11 @@ let print_point_list = fun point_list ->
   Printf.printf "]\n";;
 
 
-print_point_list (nextpos departure arrival a_trois_vingt dt);;
+print_point_list (not_in_clouds (nextpos departure arrival a_trois_vingt dt) clouds);;
 
 (*A coder : position suivante pour aller directement à l'arrivée*)
-(*A coder : intersection avec nuage*)
+(*A coder : intersection avec nuage-finir primitive*)
 (*A coder : génération d'une trajectoire naïve*)
 (*A coder : affichage*)
+(*A coder : fonction qui calcule le nouveau vecteur vitesse à l'issue d'un virage*)
 (*Attention au test d'arrivée : il ne peut pas être exact*)
